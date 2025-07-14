c755b2f33b957625b99d684ae5a4b0d6a8492cf3
radhey355 <saran35515@gmail.com>
2025-07-15 00:04:33 +0530

    window handles modfication

--- DemoTest/src/com/pages/Windows_page.java
+++ DemoTest/src/com/pages/Windows_page.java
@@ -1,9 +1,12 @@
 package com.pages;
 
 import java.time.Duration;
+import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +20,7 @@ public class Windows_page {
 	Actions action;
 	JavascriptExecutor js;
 	WebDriverWait wait;
+	String parent_Window;
 
 	@BeforeClass
 	public void SetUp() {
@@ -27,16 +31,48 @@ public class Windows_page {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-		
+		parent_Window = driver.getWindowHandle();
+
 	}
 
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
-	
-	@Test
-	public void n() {
-		System.out.println("open");
+
+	@Test(priority = 1)
+	public void tabs1() {
+		WebElement tabButton = driver.findElement(By.xpath("//*[@id = \"tabButton\"]"));
+		tabButton.click();
+		System.out.println("tabs1()");
+
 	}
+
+	@Test(priority = 2)
+	public void tab2() {
+		driver.navigate().to("https://demoqa.com/browser-windows");;
+		WebElement windowButton = driver.findElement(By.xpath("//*[@id = \"windowButton\"]"));
+		windowButton.click();
+		System.out.println("tabs2()");
+	}
+
+	@Test(priority = 3)
+	public void tab3() {
+		WebElement messageWindowButton = driver.findElement(By.xpath("//*[@id = \"messageWindowButton\"]"));
+		messageWindowButton.click();
+		System.out.println("tabs3()");
+	}
+
+	@Test(priority = 4)
+	public void allWindows_handels() {
+		Set<String> new_Window = driver.getWindowHandles();
+		for (String allWindows : new_Window) {
+			if (!allWindows.equals(parent_Window)) {
+				driver.switchTo().window(allWindows);
+				System.out.println("windoHandels");
+			}
+		}
+
+	}
+
 }
